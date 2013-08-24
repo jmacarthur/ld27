61f89b601082049237a6485cba6576012f788cc1
Jim MacArthur <jim-git@mode7.co.uk>
2013-08-24 21:08:41 +0100

    More DB work; login.pl now works correctly

--- api/login.pl
+++ api/login.pl
@@ -14,17 +14,62 @@ my $q = CGI->new;
 print $q->header('text/plain');
 print "USERID: $userID\n";
 
+sub addUserToShard
+{
+    my($shardID, $userID) = @_;
+    my $sth = $dbh->prepare("UPDATE userids SET shard=? WHERE userid=?");
+    my $rh = $sth->execute($shardID, $userID) or warn "Couldn't add user to shard\n";
+
+}
+
+sub countUsersInShard
+{
+    my $shardID = shift;
+    my $sth = $dbh->prepare("SELECT COUNT(userid) from userids where shard=?");
+    my $rh = $sth->execute($shardID);
+    my @array = $sth->fetchrow_array();
+    if(@array > 0) {
+        print "Found $array[0] users in shard.\n";
+        return $array[0];
+    }
+    warn "Can't count users in shard '$shardID'\n";
+}
+
+sub setShardUser
+{
+    my($shardID, $userID) = @_;
+    my $sth = $dbh->prepare("UPDATE shard SET nextuser=? WHERE shardid=?");
+    my $rh = $sth->execute($userID,$shardID);
+    print "Next user of shard $shardID has been set to $userID\n";
+}
+
+sub setShardStatus
+{
+    my($shardID, $newStatus) = @_;
+    my $sth = $dbh->prepare("UPDATE shard SET status=? WHERE shardid=?");
+    my $rh = $sth->execute($newStatus,$shardID) or warn "Can't update shard $shardID status to $newStatus\n";
+
+    print "Shard $shardID has been set to status $newStatus\n";
+}
+
+
 # I should also give you a shard ID. Either you find a shard with
 # 2 people in it already, or you join a shard with not enough people,
 # or make a new shard.
 
 # Is there any shard with 2 people in it?
-my $sth = $dbh->prepare("select shardid from shard where status=0 LIMIT 1;");
+my $sth = $dbh->prepare("select shardid from shard where status=0;");
 my $tuples = $sth->execute();
-my $array = $sth->fetchrow_array();
+my @array = $sth->fetchrow_array();
 
-if($array > 0) {
-    print "You join exisiting shard $array[0]\n";
+if(@array > 0) {
+    my $shardID = $array[0];
+    addUserToShard($shardID, $userID);
+    my $userCount = countUsersInShard($shardID);
+    if($userCount >= 3) {
+        setShardStatus($shardID, 1);
+    }
+    print "You join exisiting shard '$shardID', which now has $userCount users.\n";
 }
 else
 {
@@ -32,4 +77,8 @@ else
     my $rh = $sth->execute();
     my $shardID = $dbh->func('last_insert_rowid');
     print "You join new shard $shardID\n";
+    addUserToShard($shardID, $userID);
+    setShardUser($shardID, $userID);
 }
+
+# That's done
--- makeDB.pl
+++ makeDB.pl
@@ -9,7 +9,7 @@ my $dbh = DBI->connect("dbi:SQLite:condition.sqlite","","");
 my $sth = $dbh->prepare("CREATE TABLE userids (userid integer primary key autoincrement, shard integer);");
 my $rv = $sth->execute() or die $sth->errstr;
 
-$sth = $dbh->prepare("CREATE TABLE shard (shardid integer primary key autoincrement, status integer, playerx integer, playery integer, time integer);");
+$sth = $dbh->prepare("CREATE TABLE shard (shardid integer primary key autoincrement, status integer, playerx integer, playery integer, time integer, nextuser integer);");
 $rv = $sth->execute() or die $sth->errstr;
 
 
