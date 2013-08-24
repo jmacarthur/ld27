944730917a65c41ae3d5be73b3c34e636c80bb31
Jim MacArthur <jim-github@mode7.co.uk>
2013-08-24 10:32:44 +0100

    Added key functions, circle moves in response to arrow keys

--- condition.js
+++ condition.js
@@ -2,9 +2,28 @@ var canvas = document.getElementsByTagName('canvas')[0],
 ctx = null,
 body = document.getElementsByTagName('body')[0];
 var frame = 0;
-
+var keysDown = new Array();
+var x;
+var y;
 function init() {
-    // Does nothing
+    x = 320
+    y = 240
+}
+
+function animate() {
+    if(keysDown[38]) {
+	y -= 1;
+    }
+    else if(keysDown[40]) {
+	y += 1;
+    }
+    if(keysDown[37]) {
+	x -= 1;
+    }
+    else if(keysDown[39]) {
+	x += 1;
+    }
+
 }
 
 function draw() {
@@ -14,12 +33,13 @@ function draw() {
 
     ctx.beginPath();
     ctx.fillStyle = "#ff0000";  
-    ctx.arc(320,240,32,0,2*Math.PI,false);
+    ctx.arc(x,y,32,0,2*Math.PI,false);
     ctx.fill();
 }
 
 
 function drawRepeat() {
+    animate();
     draw();
     setTimeout('drawRepeat()',10);
 }
@@ -39,7 +59,17 @@ if (canvas.getContext('2d')) {
 	mousey = event.clientY - canvas.offsetTop;
 	// Does nothing else
     };
-   
+    body.onkeydown = function (event) {
+	var c = event.keyCode;
+        keysDown[c] = 1;
+        console.log("Pressed key: "+c);
+    };
+
+    body.onkeyup = function (event) {
+	var c = event.keyCode;
+        keysDown[c] = 0;
+    };
+
     // Clear canvas
     ctx.fillStyle = "#000000";
     ctx.fillRect(0,0,640,480);
